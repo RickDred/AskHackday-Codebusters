4198b1b6e90282c02d66aa7525ecb72576608e42
RickDred <newakkism@gmail.com>
2022-06-17 11:16:16 +0600

    University

---
+++ app/src/main/java/com/snotshot/codebusters/WelcomeActivity.kt
@@ -0,0 +1,10 @@
+package com.snotshot.codebusters
+
+import androidx.appcompat.app.AppCompatActivity
+import com.snotshot.codebusters.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityWelcomeBinding
+
+
+}
